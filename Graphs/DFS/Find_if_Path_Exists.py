562f591d6576e0a0472b87d8e0780e6ed63baba8
Nahili <nahili.ansha@mnsu.edu>
2025-07-12 13:01:38 -0400

    Find-if-Path-Exists-in-Graph

---
+++ Graphs/DFS/Find_if_Path_Exists.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+
+# Find if Path Exists in Graph
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        def dfs(node):
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+        if not edges:
+            return True
+
+        graph = defaultdict(list)
+        seen = set()
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+        dfs(source)         
+        if destination in seen:
+            return True
+        else:
+            return False        
